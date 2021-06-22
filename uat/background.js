d3815fc8a691193b7ff0c6c855409f80528b0b22
LHO-Developments <86275688+LHO-Developments@users.noreply.github.com>
2021-06-21 22:18:12 -0500

    Delete uat directory

--- uat/background.js
+++
@@ -1,7 +0,0 @@
-chrome.contextMenus.create({
-    title: "GitHub",
-    contexts: ["browser_action"],
-    onclick: function () {
-        window.open('https://github.com/', '_blank').focus();
-    }
-});
--- uat/constants.js
+++
@@ -1,7 +0,0 @@
-/**
- * In the constants.js
- * To set the number amount of rows for the extension to be available
- * Example: 21 is only 20 rows available
- * 
- * */
-const LIMITAD = 51;
